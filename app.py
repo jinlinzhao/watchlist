b9a2e730bde897c314d0b90fdd621daaadd1dca3
zhaojinlin <791584987@qq.com>
2019-09-10 15:06:17 +0800

    add index page

--- app.py
+++ app.py
@@ -4,12 +4,11 @@
 # @Site :  
 # @File : app.py
 
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
 @app.route('/index')
 @app.route('/home')
 def hello():
@@ -30,3 +29,22 @@ def test_url_for():
     print(url_for('test_url_for'))
     print(url_for('test_url_for', num=2))
     return 'Test page'
+
+
+@app.route('/')
+def index():
+    name = 'Grey Li'
+    movies = [
+
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+    return render_template('index.html', name=name, movies=movies)
